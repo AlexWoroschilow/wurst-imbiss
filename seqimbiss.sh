7ff3f08e21dbc5f9a2f5d52dca38f889404e90b1
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-12-09 17:00:05 +0100

    =add file to compare sequences

--- seqimbiss.sh
+++ seqimbiss.sh
@@ -8,7 +8,8 @@
 # get rid of this #$ -v PATH=$path
 
 
-export LD_LIBRARY_PATH=/home/sensey/Projects/WurstImbiss/vendor/zlog/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/home/sensey/Projects/WurstImbiss/vendor/zlog/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/stud2013/ovoroshylov/Clustering/wurst-imbiss/vendor/zlog/lib:$LD_LIBRARY_PATH
 #valgrind --leak-check=full 
 ./seqimbiss.x -l 9 -l 15 -n 50
 

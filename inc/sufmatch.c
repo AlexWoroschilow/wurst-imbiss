1fe91cae88fc3ee8056b6e50f0c27d4f9f1ae81c
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-12-10 17:14:48 +0100

    =add asserts

--- inc/sufmatch.c
+++ inc/sufmatch.c
@@ -26,6 +26,7 @@
 #include "cantor.h"
 #include "wurstimbiss.h"
 #include "falphabet.h"
+#include "massert.h"
 
 /*------------------------------- suffixscore --------------------------------
  *    
@@ -492,6 +493,11 @@ void rankSufmatch(void *space, Suffixarray *suffix_array, PairSint *matches, Uin
 
 			ptr = (suffix_array->suffixptr[suffix_array->suftab[j]]);
 			const IntSequence * sequence_b = sequences[match->id];
+			massert((info != NULL), "Info can not be null");
+			massert((sequence_a != NULL), "Sequence a can not be null");
+			massert((sequence_b != NULL), "Sequence b can not be null");
+			massert((match != NULL), "Match can not be null");
+			massert((ptr != NULL), "Ptr can not be null");
 			const int response = filter(space, match, sequence_a, sequence_b, ptr, substrlen, i, info);
 			if (response == -1) {
 				break;
